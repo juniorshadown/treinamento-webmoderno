bea1c83d9c77cb64caa57bd5fa4d8c1f1f9cbfea
Junior Shadown <123junior.sousa@gmail.com>
2021-11-27 16:12:23 -0300

    pronto

---
+++ Node-js/global.js
@@ -0,0 +1,7 @@
+// console.log(global)
+global.MinhaApp = Object.freeze({
+    Saudacao(){
+        return 'estou em todos os cantos!'
+    },
+    nome: 'Sistema Seguro'
+})
---
+++ Node-js/globalCliente.js
@@ -0,0 +1,5 @@
+require('./global')
+console.log(MinhaApp.Saudacao())
+
+MinhaApp.nome = 'Eita!'
+console.log(MinhaApp.nome)
