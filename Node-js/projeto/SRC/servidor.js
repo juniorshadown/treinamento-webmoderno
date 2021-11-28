be675bf6e8a66a635fe223e2994381ad2aa6d74f
Junior Shadown <123junior.sousa@gmail.com>
2021-11-28 17:24:46 -0300

    deu

--- Node-js/projeto/SRC/servidor.js
+++ Node-js/projeto/SRC/servidor.js
@@ -2,7 +2,10 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancodedados = require('./bancodedados')
+
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/produtos', (req, res, next)=> {
     res.send(bancodedados.getProduto(req.params.id)) // converte automaticamente para JSON
@@ -14,7 +17,7 @@ app.get('/produtos/id', (req, res, next)=> {
 
 app.post('/produtos', (req, res, next)=> {
     const produto = bancodedados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto) // json pra web
