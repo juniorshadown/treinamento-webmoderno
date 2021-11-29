3f45013b9fc20d0e669864f74061e2207f992118
Junior Shadown <123junior.sousa@gmail.com>
2021-11-29 20:08:17 -0300

    boa

--- Node-js/temporizador.js
+++ Node-js/temporizador.js
@@ -1,5 +1,6 @@
 const  schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('/5******', function () {
-    console.log('executando tarefa 1!', new Date () .getSeconds ())  
+const tarefa1 = schedule.scheduleJob({rule: '*/5 * * * * *'}, function() {
+    console.log('executando tarefa 1!', new Date().getSeconds())  
 })
+
