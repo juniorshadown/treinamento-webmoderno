392ca04ca916175b71ae8d7e7ac4a54255823b77
Junior Shadown <123junior.sousa@gmail.com>
2021-11-29 23:04:05 -0300

    orrr

--- Node-js/entradaesaida.js
+++ Node-js/entradaesaida.js
@@ -2,5 +2,14 @@ const anonimo = process.argv.indexOf('-a') !== -1
 // console.log(anonimo)
 
 if(anonimo) {
-    process.stdout.write('Fala anonimo!\n')
+    process.stdout.write('Fala anonimo!\r') // saida padrao..\r quebra de linha
+    process.exit()
+}else { // caso a flag anonima nao esteja presente
+    process.stdout.write('informe seu nome: ')
+    process.stdin.on('data', data =>{ //stdin (teclado)
+        const nome = data.toString().replace('\r\n', ''); // '\r\n'  que é a quebra de linha no Windows. // salvando na const o nome que foi digitado... replace remover o enter que o usuario apertou
+
+        process.stdout.write(`Fala ${nome}!!`)
+        process.exit()
+    })
 }
--- Node-js/temporizador.js
+++ Node-js/temporizador.js
@@ -4,3 +4,4 @@ const tarefa1 = schedule.scheduleJob({rule: '*/5 * * * * *'}, function() {
     console.log('executando tarefa 1!', new Date().getSeconds())  
 })
 
+// essa função da a mensagem de 5 em 5 segundos. > node-Schedule <
