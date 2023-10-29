36b471a1c297b35d5be0e9e242cebed4e1629ed5
juniorshadown <87620516+juniorshadown@users.noreply.github.com>
2023-10-29 15:39:31 -0300

    OK

---
+++ iniciopartsetenc/desafioarea.js
@@ -0,0 +1,5 @@
+let raio = 10
+const PI = 3.14
+let areaCircu = PI * raio * raio;
+console.log("O valor da area é = " + areaCircu);
+console.log(Math.PI);
---
+++ iniciopartsetenc/desafiotrocdvlores.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+
+[a, b] = [b, a]
+//let temp = a;
+//a = b;
+//b = temp
+//a = 94
+//b = 7
+console.log(a)
+console.log(b)
